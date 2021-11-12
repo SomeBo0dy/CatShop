7265be65f0d6be5f1abb9470b99adec215493bf2
xyjyyds <979084805@qq.com>
2021-11-12 23:39:21 +0800

    0.0

--- src/text2/Test1.java
+++ src/text2/Test1.java
@@ -6,18 +6,19 @@ package text2;
 public class Test1 {
     public static void main(String[] args) {
         int[] arr1={1,3,5,7,9};
-        int[] arr2={2,4,6,8,10};
+        int[] arr2={2,4,6,8,10,11,12,14};
         out(arr1,arr2);
     }
     public static void out(int[] arr1, int[] arr2){
         final Object lock = new Object();
         Thread t1 = new Thread(() -> {
             synchronized (lock){
-                for(int num : arr1){
-                    System.out.print(num + " ");
+                for(int i = 0;i < arr1.length; i++){
+                    System.out.print(arr1[i] + " ");
                     try {
                         lock.notifyAll();
-                        lock.wait();
+                        if(i < arr2.length)
+                            lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
@@ -28,11 +29,12 @@ public class Test1 {
 
         Thread t2 = new Thread(() -> {
             synchronized (lock){
-                for(int num : arr2){
-                    System.out.print(num + " ");
+                for(int i = 0;i < arr2.length; i++){
+                    System.out.print(arr2[i] + " ");
                     try {
                         lock.notifyAll();
-                        lock.wait();
+                        if(i < arr1.length)
+                            lock.wait();
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
---
+++ src/text3/Test1.java
@@ -0,0 +1,9 @@
+package text3;
+
+/**
+ * @author xyj
+ * @create 2021-11-12 23:26
+ */
+public class Test1 {
+
+}
